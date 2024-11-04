dd2735b35b2e44814e8fa11a32d460caa254ddb4
nguyenlong1019 <nguyenvanlong17052002@gmail.com>
2024-11-04 17:53:30 +0700

    Fix Enrollments and Classrooms

--- server/cms_server/core/models/classrooms.py
+++ server/cms_server/core/models/classrooms.py
@@ -3,9 +3,21 @@ from cms_server.utils import CommonAbstract
 
 
 class Classrooms(CommonAbstract):
+    ROOM = (
+        (0, 'Phòng lý thuyết'),
+        (1, 'Phòng thực hành')
+    )
+
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    room_code = models.CharField(max_length=20, verbose_name='Mã phòng')
     room_number = models.CharField(max_length=15, verbose_name='Số phòng')
     capacity = models.IntegerField(default=40, verbose_name='Sức chứa')
+    table_qty = models.IntegerField(default=10, verbose_name='Số bàn')
+    slot_qty = models.IntegerField(default=40, verbose_name='Số chỗ ngồi')
+    room_type = models.IntegerField(default=0, choices=ROOM, verbose_name='Loại phòng')
+    note = models.TextField(null=True, blank=True, max_length=500, verbose_name='Ghi chú')
+    has_projector = models.BooleanField(default=True, verbose_name='Máy chiếu')
+    square = models.FloatField(default=50, verbose_name='Diện tích')
 
 
     class Meta:
--- server/cms_server/core/models/enrollments.py
+++ server/cms_server/core/models/enrollments.py
@@ -8,7 +8,12 @@ class Enrollment(CommonAbstract):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, verbose_name='Sinh viên')
     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, verbose_name='Học phần đăng ký')
-
+    cost = models.IntegerField(default=0, verbose_name='Mức học phí')
+    is_cost_done = models.BooleanField(default=False, verbose_name='Đã đóng học phí')
+    reg_date = models.DateField(verbose_name='Ngày đăng ký')
+    note = models.TextField(null=True, blank=True, max_length=500, verbose_name='Ghi chú')
+    principal_cost = models.IntegerField(default=0)
+    practice_cost = models.IntegerField(default=0)
 
     class Meta:
         verbose_name = 'Đăng ký học phần'
